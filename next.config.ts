2588f770b5cb6184f10ee2aa72c50a7ba6054cef
BrianKimeli <brayokimeli@gmail.com>
2026-05-31 03:48:58 -0700

    refactor: merge custom domain rewrites into nextconfig

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,58 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async redirects() {
     return [
-      { source: "/blog", destination: "/times", permanent: true },
+      { 
+        source: "/blog", 
+        destination: "/times", 
+        permanent: true 
+      },
+      {
+        source: '/times',
+        has: [
+          {
+            type: 'host',
+            value: 'shilingiapp.vercel.app',
+          },
+        ],
+        destination: 'https://shilingitimes.vercel.app',
+        permanent: false,
+      },
+      {
+        source: '/times/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'shilingiapp.vercel.app',
+          },
+        ],
+        destination: 'https://shilingitimes.vercel.app/:path*',
+        permanent: false,
+      },
+    ];
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'shilingitimes.vercel.app',
+          },
+        ],
+        destination: '/times',
+      },
+      {
+        source: '/:path((?!_next/static|_next/image|favicon.ico|api).*)',
+        has: [
+          {
+            type: 'host',
+            value: 'shilingitimes.vercel.app',
+          },
+        ],
+        destination: '/times/:path*',
+      },
     ];
   },
 };
